ac5c2fdfdb5aa76b5ebba26509b7d4f27be7f776
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-22 23:54:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*!
+ * strip-noncharacters <https://github.com/jonschlinkert/strip-noncharacters>
+ *
+ * Copyright (c) 2015 Jon Schlinkert.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var regex = require('noncharacter-regex');
+
+module.exports = function (str) {
+  if (typeof str !== 'string') {
+    throw new Error('strip-noncharacters expects a string');
+  }
+  return str.replace(regex, '');
+};

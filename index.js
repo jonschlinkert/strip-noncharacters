faa5373eed6b1545e3301aa0d47dfacb2a971e8f
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-27 10:14:59 -0500

    run update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * strip-noncharacters <https://github.com/jonschlinkert/strip-noncharacters>
  *
- * Copyright (c) 2015 Jon Schlinkert.
- * Licensed under the MIT license.
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
